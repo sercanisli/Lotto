df1104096db19e68287e2fa4072995c2fa9c3a5a
Sercan ISLI <sercanislii@gmail.com>
2023-10-24 01:52:01 +0300

    SuperLoto DTO For Update added.

---
+++ Entities/DataTransferObjects/SuperLotoDtoForUpdate.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.DataTransferObjects
+{
+    public record SuperLotoDtoForUpdate
+    {
+        public int Id { get; init; }
+        public List<int> Numbers { get; init; }
+
+        public SuperLotoDtoForUpdate()
+        {
+            Numbers = new List<int>();
+        }
+    }
+}
