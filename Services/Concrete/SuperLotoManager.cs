234b895add791f7139dcb53c61df66f4684686ea
Sercan ISLI <sercanislii@gmail.com>
2023-10-23 17:09:00 +0300

    Services layer has been created for Super Loto

---
+++ Services/Concrete/SuperLotoManager.cs
@@ -0,0 +1,65 @@
+﻿using Entities.Models;
+using Repositories.Cantracts;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+    public class SuperLotoManager : ISuperLotoService
+    {
+        private readonly IRepositoryManager _manager;
+
+        public SuperLotoManager(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+        public SuperLoto CreateOneNumbersArray(SuperLoto superLoto)
+        {
+            if(superLoto == null)
+            {
+                throw new ArgumentNullException(nameof(superLoto));
+            }
+            _manager.SuperLoto.CreateOneNumbersArray(superLoto);
+            _manager.Save();
+            return superLoto;
+        }
+
+        public void DeleteOneNumbersArray(int id, bool trackChanges)
+        {
+            var entity = _manager.SuperLoto.GetOneNumbersArrayById(id, false);
+            if(entity == null) 
+            {
+                throw new Exception($"Super Loto wit id: {id} could not found.");
+            }
+            _manager.SuperLoto.DeleteOneNumbersArray(entity);
+            _manager.Save();
+        }
+
+        public IEnumerable<SuperLoto> GetAllNumbersArrays(bool trackChanges)
+        {
+            return _manager.SuperLoto.GetAllNumbersArray(trackChanges); 
+        }
+
+        public SuperLoto GetOneNumbersArrayById(int id, bool trackChanges)
+        {
+            return _manager.SuperLoto.GetOneNumbersArrayById(id, trackChanges);
+        }
+
+        public void UpdateOneNumbersArray(int id, SuperLoto superLoto, bool trackChanges)
+        {
+            var entity = _manager.SuperLoto.GetOneNumbersArrayById(id, false);
+            if (entity == null)
+            {
+                throw new Exception($"Super Loto wit id: {id} could not found.");
+            }
+            if (superLoto == null)
+            {
+                throw new ArgumentNullException(nameof(superLoto));
+            }
+            entity.Numbers=superLoto.Numbers;
+
+            _manager.SuperLoto.UpdateOneNubersArray(entity);
+            _manager.Save();
+        }
+    }
+}
---
+++ Services/Contracts/ISuperLotoService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+
+namespace Services.Contracts
+{
+    public interface ISuperLotoService
+    {
+        IEnumerable<SuperLoto> GetAllNumbersArrays(bool trackChanges);
+        SuperLoto GetOneNumbersArrayById(int id, bool trackChanges);
+        SuperLoto CreateOneNumbersArray(SuperLoto superLoto);
+        void UpdateOneNumbersArray(int id, SuperLoto superLoto, bool trackChanges);
+        void DeleteOneNumbersArray(int id, bool trackChanges);
+
+    }
+}
