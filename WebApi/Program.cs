90935aee861f894c90dc32d1805b7aa385b10e61
Sercan ISLI <sercanislii@gmail.com>
2023-10-23 18:12:18 +0300

    nlog.config folder has been added. Configurations were made for logging.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,9 @@
+using NLog;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
@@ -10,6 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
