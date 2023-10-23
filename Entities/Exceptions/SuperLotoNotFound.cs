0b9bd97dd95a2b2e12276597cd0138db7eb4cd7a
Sercan ISLI <sercanislii@gmail.com>
2023-10-23 18:55:57 +0300

    custom nor found class created.

---
+++ Entities/Exceptions/SuperLotoNotFound.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class SuperLotoNotFound : NotFound
+    {
+        public SuperLotoNotFound(string id) : base($"SuperLoto wit id : {id} could not found.")
+        {
+        }
+    }
+}
