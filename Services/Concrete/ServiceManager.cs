32104e890392ffddde13f334fe0600e7cbb543da
Sercan ISLI <sercanislii@gmail.com>
2023-10-23 17:13:14 +0300

    Service Manager has been created. Lazy loading applied.

---
+++ Services/Concrete/ServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using Repositories.Cantracts;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ISuperLotoService> _superLotoService;
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _superLotoService = new Lazy<ISuperLotoService>(() => new SuperLotoManager(repositoryManager));
+        }
+        public ISuperLotoService SuperLoto => _superLotoService.Value;
+    }
+}
