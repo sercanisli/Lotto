7f904e9a507df50ca52a0810d37594088c632b8a
Sercan ISLI <sercanislii@gmail.com>
2023-10-30 01:45:53 +0300

    Link Response entity was created for link organisation.

---
+++ Entities/LinkModels/LinkResponse.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+
+namespace Entities.LinkModels
+{
+    public class LinkResponse
+    {
+        public bool HasLinks { get; set; }
+        public List<Entity> ShapedEntity { get; set; }
+        public LinkCollectionWrapper<Entity> LinkedEntities { get; set; }
+
+        public LinkResponse()
+        {
+            ShapedEntity = new List<Entity>();
+            LinkedEntities = new LinkCollectionWrapper<Entity>();   
+        }
+    }
+}
