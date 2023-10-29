f57de497b36e3516298b539c7d50f7fbf655b1f6
Sercan ISLI <sercanislii@gmail.com>
2023-10-29 23:45:26 +0300

    Link entity was created for hyper media support.

---
+++ Entities/LinkModels/Link.cs
@@ -0,0 +1,21 @@
+﻿namespace Entities.LinkModels
+{
+    public class Link
+    {
+        public string? Href { get; set; }
+        public string? Relation { get; set; }
+        public string? Method { get; set; }
+
+        public Link()
+        {
+            
+        }
+
+        public Link(string? href, string? relation, string? method)
+        {
+            Href = href;
+            Relation = relation;
+            Method = method;
+        }
+    }
+}
