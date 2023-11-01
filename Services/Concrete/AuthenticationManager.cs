3028f60e97bf4a022180e5fbddf8a2f57e8b312a
Sercan ISLI <sercanislii@gmail.com>
2023-11-01 17:36:26 +0300

    IAuthenticationService and AuthenticationManager have been created.

---
+++ Services/Concrete/AuthenticationManager.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+    public class AuthenticationManager : IAuthenticationService
+    {
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserDtoForRegistration userDtoForRegistration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
