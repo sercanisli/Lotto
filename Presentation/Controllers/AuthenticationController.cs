8debcecef8903794ea420777af5695369fce7d47
Sercan ISLI <sercanislii@gmail.com>
2023-11-01 18:08:42 +0300

    Authentication Controller was created.

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Services.Contracts;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public AuthenticationController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserDtoForRegistration userDtoForRegistration)
+        {
+            var result = await _manager.AuthenticationService.RegisterUser(userDtoForRegistration);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return StatusCode(201);
+        }
+    }
+}
