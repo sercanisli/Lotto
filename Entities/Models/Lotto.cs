42ae0aab1c5c97fb55c373253ee68776639936d5
Sercan ISLI <sercanislii@gmail.com>
2023-11-03 00:07:22 +0300

    A generic class named Lotto was defined. SuperLotto and the newly created NumericLotto classes were implemented from this class.

---
+++ Entities/Models/Lotto.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Entities.Models
+{
+    public class Lotto
+    {
+        public int Id { get; set; }
+        public List<int> Numbers { get; set; }
+        public DateTime? Date { get; set; }
+
+        public Lotto()
+        {
+            Numbers = new List<int>();
+        }
+
+        public Lotto(DateTime date) : this()
+        {
+            Date = date;
+        }
+
+        public Lotto(string dateString) : this()
+        {
+            if (!string.IsNullOrWhiteSpace(dateString))
+            {
+                DateTime parsedDate;
+                if (DateTime.TryParseExact(dateString, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
+                {
+                    Date = parsedDate;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid date format.", nameof(dateString));
+                }
+            }
+        }
+    }
+}
--- Entities/Models/SuperLoto.cs
+++ Entities/Models/SuperLoto.cs
@@ -2,36 +2,7 @@
 
 namespace Entities.Models
 {
-    public class SuperLoto
+    public class SuperLoto : Lotto
     {
-        public int Id { get; set; }
-        public List<int> Numbers { get; set; }
-        public DateTime? Date { get; set; }
-
-        public SuperLoto()
-        {
-            Numbers = new List<int>();
-        }
-
-        public SuperLoto(DateTime date) : this()
-        {
-            Date = date;
-        }
-
-        public SuperLoto(string dateString) : this()
-        {
-            if (!string.IsNullOrWhiteSpace(dateString))
-            {
-                DateTime parsedDate;
-                if (DateTime.TryParseExact(dateString, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedDate))
-                {
-                    Date = parsedDate;
-                }
-                else
-                {
-                    throw new ArgumentException("Invalid date format.", nameof(dateString));
-                }
-            }
-        }
     }
 }
