16d7c3cbf356df63c3360a9212f9136abfb64f67
Sercan ISLI <sercanislii@gmail.com>
2023-11-05 00:47:56 +0300

    IoC registrations of Repositories for SayisalLoto object were made in RepositoryManager class with Lazy loading method.

--- Repositories/Contract/IRepositoryManager.cs
+++ Repositories/Contract/IRepositoryManager.cs
@@ -1,8 +1,11 @@
-﻿namespace Repositories.Cantracts
+﻿using Repositories.Contract;
+
+namespace Repositories.Cantracts
 {
     public interface IRepositoryManager
     {
         ISuperLotoRepository SuperLoto { get; }
+        ISayisalLotoRepository SayisalLoto { get; }
 
         Task SaveAsync();
     }
--- Repositories/EntityFrameworkCore/RepositoryManager.cs
+++ Repositories/EntityFrameworkCore/RepositoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using Repositories.Cantracts;
+using Repositories.Contract;
 
 namespace Repositories.EntityFrameworkCore
 {
@@ -6,14 +7,18 @@ namespace Repositories.EntityFrameworkCore
     {
         private readonly RepositoryContext _context;
         private readonly Lazy<ISuperLotoRepository> _superLotoRepository;
+        private readonly Lazy<ISayisalLotoRepository> _sayisalLotoRepository;
 
         public RepositoryManager(RepositoryContext context)
         {
             _context = context;
             _superLotoRepository = new Lazy<ISuperLotoRepository>(() => new SuperLotoRepository(_context));
+            _sayisalLotoRepository = new Lazy<ISayisalLotoRepository>(() => new SayisalLotoRepository(_context));
         }
 
         public ISuperLotoRepository SuperLoto => _superLotoRepository.Value;
+
+        public ISayisalLotoRepository SayisalLoto => _sayisalLotoRepository.Value;
 
         public async Task SaveAsync() => await _context.SaveChangesAsync();
     }
