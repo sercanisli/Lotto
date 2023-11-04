35a0805c8b9a9d7f1a06933f8fe30905ebae982a
Sercan ISLI <sercanislii@gmail.com>
2023-11-05 00:39:33 +0300

    Repository definitions were made for SayisalLoto entity

---
+++ Repositories/Contract/ISayisalLotoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Repositories.Cantracts;
+
+namespace Repositories.Contract
+{
+    public interface ISayisalLotoRepository : IRepositoryBase<SayisalLoto>
+    {
+        IQueryable<SayisalLoto> GetAllNumbersArrayAsync(bool trackChanges);
+        IQueryable<SayisalLoto> GetOneNumbersArrayByIdAsync(int id, bool trackChanges);
+        void CreateOneNumbersArray(SayisalLoto sayisalLoto);
+        void UpdateOneNumbersArray(SayisalLoto sayisalLoto);
+        void DeleteOneNumbersArray(SayisalLoto sayisalLoto);
+    }
+}
---
+++ Repositories/EntityFrameworkCore/SayisalLotoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class SayisalLotoRepository : RepositoryBase<SayisalLoto>, ISayisalLotoRepository
+    {
+        public SayisalLotoRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateOneNumbersArray(SayisalLoto sayisalLoto) => Create(sayisalLoto);
+
+        public void DeleteOneNumbersArray(SayisalLoto sayisalLoto) => Delete(sayisalLoto);
+        public IQueryable<SayisalLoto> GetAllNumbersArrayAsync(bool trackChanges) =>
+            FindAll(trackChanges).OrderBy(sl=>sl.Numbers);
+        public IQueryable<SayisalLoto> GetOneNumbersArrayByIdAsync(int id, bool trackChanges) =>
+            FindByCondition(sl => sl.Id.Equals(id), trackChanges);
+
+        public void UpdateOneNumbersArray(SayisalLoto sayisalLoto) => Update(sayisalLoto);
+    }
+}
