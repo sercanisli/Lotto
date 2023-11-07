85c190fdd335cf034e13f1cbb739d1406d58229a
Sercan ISLI <sercanislii@gmail.com>
2023-11-07 17:29:23 +0300

    Custom SayisalLotoNotFoundException class has been created.

---
+++ Entities/Exceptions/SayisalLotoNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class SayisalLotoNotFoundException : NotFoundException
+    {
+        public SayisalLotoNotFoundException(int id) : base($"The Sayisal Loto with id : {id} could not found.")
+        {
+        }
+    }
+}
