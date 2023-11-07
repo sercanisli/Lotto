f75be43829bf7c7642c22001264bcba4f742505b
Sercan ISLI <sercanislii@gmail.com>
2023-11-07 18:07:43 +0300

    SayisalLotoDtoForUpdate created.

---
+++ Entities/DataTransferObjects/SayisalLotoDtoForUpdate.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.DataTransferObjects
+{
+    public record SayisalLotoDtoForUpdate
+    {
+        public int Id { get; init; }
+        public List<int> Numbers { get; init; }
+        public DateTime Date { get; init; }
+
+        public SayisalLotoDtoForUpdate()
+        {
+            Numbers = new List<int>();
+        }
+    }
+}
