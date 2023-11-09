606f69196525c7d20f6344a18261019faf5de80e
Sercan ISLI <sercanislii@gmail.com>
2023-11-09 13:11:14 +0300

    For the paging structure, the SayisalLotoParameters class was created and implemented from the RequestParameters class.

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -3,7 +3,9 @@
     public abstract class RequestParameters
     {
         const int maxPageSize = 10;
+
         public int PageNumber { get; set; }
+
         private int _pageSize;
 
 		public int PageSize
