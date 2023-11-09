a653b5c991afbb6f4c6d03cf284f39151647c8ad
Sercan ISLI <sercanislii@gmail.com>
2023-11-09 13:28:27 +0300

    Refactoring was applied in SayisalLotoRepository for the paging structure.

--- Repositories/Contract/ISayisalLotoRepository.cs
+++ Repositories/Contract/ISayisalLotoRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Repositories.Cantracts;
 
 namespace Repositories.Contract
 {
     public interface ISayisalLotoRepository : IRepositoryBase<SayisalLoto>
     {
-        Task<IEnumerable<SayisalLoto>> GetAllNumbersArrayAsync(bool trackChanges);
+        Task<IEnumerable<SayisalLoto>> GetAllNumbersArrayAsync(SayisalLotoParameters sayisalLotoParameters, bool trackChanges);
         Task<SayisalLoto> GetOneNumbersArrayByIdAsync(int id, bool trackChanges);
         void CreateOneNumbersArray(SayisalLoto sayisalLoto);
         void UpdateOneNumbersArray(SayisalLoto sayisalLoto);
--- Repositories/EntityFrameworkCore/SayisalLotoRepository.cs
+++ Repositories/EntityFrameworkCore/SayisalLotoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Contract;
 
@@ -13,8 +14,14 @@ namespace Repositories.EntityFrameworkCore
         public void CreateOneNumbersArray(SayisalLoto sayisalLoto) => Create(sayisalLoto);
 
         public void DeleteOneNumbersArray(SayisalLoto sayisalLoto) => Delete(sayisalLoto);
-        public async Task<IEnumerable<SayisalLoto>> GetAllNumbersArrayAsync(bool trackChanges) =>
-            await FindAll(trackChanges).OrderBy(sl=>sl.Numbers).ToListAsync();
+
+        public async Task<IEnumerable<SayisalLoto>> GetAllNumbersArrayAsync(SayisalLotoParameters sayisalLotoParameters, bool trackChanges) =>
+            await FindAll(trackChanges)
+            .OrderBy(sl=>sl.Numbers)
+            .Skip((sayisalLotoParameters.PageNumber-1)*sayisalLotoParameters.PageSize)
+            .Take(sayisalLotoParameters.PageSize)
+            .ToListAsync();
+
         public async Task<SayisalLoto> GetOneNumbersArrayByIdAsync(int id, bool trackChanges) =>
             await FindByCondition(sl => sl.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
 
