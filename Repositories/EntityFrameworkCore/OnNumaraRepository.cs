25102f308680a4df39311b751b0c0bc7df6bbb33
Sercan ISLI <sercanislii@gmail.com>
2023-11-15 16:21:58 +0300

    IOnNumaraRepository and OnNumaraRepository were created.  They are implemented ftom the Generic Repository class and interface.

---
+++ Repositories/EntityFrameworkCore/OnNumaraRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Models;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class OnNumaraRepository : RepositoryBase<OnNumara>, IOnNumaraRepository
+    {
+        public OnNumaraRepository(RepositoryContext context) : base(context)
+        {
+        }
+    }
+}
