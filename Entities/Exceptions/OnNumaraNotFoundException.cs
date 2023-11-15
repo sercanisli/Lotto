ce7a6362bf451087485806fcfdf666761bc470f2
Sercan ISLI <sercanislii@gmail.com>
2023-11-16 01:24:40 +0300

    A Global OnNumberFoundException class has been created for the case that the related OnNumber array cannot be found.

---
+++ Entities/Exceptions/OnNumaraNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class OnNumaraNotFoundException : NotFoundException
+    {
+        public OnNumaraNotFoundException(int id) : base($"The On Numara with id : {id} could not found.")
+        {
+        }
+    }
+}
