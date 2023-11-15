28942466eef6670a7a7f6f40672b90008472eed8
Sercan ISLI <sercanislii@gmail.com>
2023-11-15 17:26:51 +0300

    Created method signatures for OnNumberManager.

---
+++ Services/Contracts/IOnNumaraService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+
+namespace Services.Contracts
+{
+    public interface IOnNumaraService
+    {
+        IEnumerable<OnNumara> GetAllNumbersArraysAsync(bool trackChanges);
+        OnNumara GetOneNumbersArrayByIdAsync(int id, bool trackChanges);
+        OnNumara CreateOneNumbersArrayAsync(OnNumara onNumara);
+        void UpdateOneNumbersArrayAsync(int id, OnNumara onNumara);
+        void DeleteOneNumbersArrayAsync(int id, bool trackChanges);
+    }
+}
