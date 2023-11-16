4aafaf214c420454c376edc81682cb2a9373a5c0
Sercan ISLI <sercanislii@gmail.com>
2023-11-17 02:08:45 +0300

    OnNumaraDtoForInsertion has been created and implemented from OnNumaraDtoForManipulation.

---
+++ Entities/DataTransferObjects/OnNumaraDtoForInsertion.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Validations;
+
+namespace Entities.DataTransferObjects
+{
+    public record OnNumaraDtoForInsertion : OnNumaraDtoForManipulation
+    {
+        public string Date { get; init; }
+        public OnNumaraDtoForInsertion()
+        {
+            
+        }
+    }
+}
