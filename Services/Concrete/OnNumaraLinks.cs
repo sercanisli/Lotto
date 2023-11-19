8b7c7d81fe345398c4bc5d0157684ef17053549c
Sercan ISLI <sercanislii@gmail.com>
2023-11-20 02:45:43 +0300

    For Hyper Media support OnNumaraLink class is created and implemented generically from ILotoLinks interface and LotoLinks class.

---
+++ Services/Concrete/OnNumaraLinks.cs
@@ -0,0 +1,13 @@
+﻿using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Routing;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+    public class OnNumaraLinks : LotoLinks<OnNumaraDto>, IOnNumaraLinks
+    {
+        public OnNumaraLinks(LinkGenerator linkGenerator, IDataShaper<OnNumaraDto> dataShaper) : base(linkGenerator, dataShaper)
+        {
+        }
+    }
+}
