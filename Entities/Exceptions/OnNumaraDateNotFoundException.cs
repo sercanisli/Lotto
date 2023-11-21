3e56031bb9d104bcdfb5f40ec9ce8f9214bc6f42
Sercan ISLI <sercanislii@gmail.com>
2023-11-22 02:21:32 +0300

    Custom exception class was created for OnNumber not found for the selected date.

---
+++ Entities/Exceptions/OnNumaraDateNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public class OnNumaraDateNotFoundException : NotFoundException
+    {
+        public OnNumaraDateNotFoundException(DateTime date) : base($"On Numara with date : {date} could not found.")
+        {
+        }
+    }
+}
