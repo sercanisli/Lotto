20731016f294a245361a1e58bdd52e1198adb910
Sercan ISLI <sercanislii@gmail.com>
2023-11-23 16:02:52 +0300

    Method signatures have been defined for SansTopu.

---
+++ Repositories/Contract/ISansTopuRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Repositories.Cantracts;
+
+namespace Repositories.Contract
+{
+    public interface ISansTopuRepository : IRepositoryBase<SansTopu>
+    {
+        IQueryable<SansTopu> GetAllNumbersArray(bool trackChanges);
+        SansTopu GetOneNumbersArrayById(bool trackChanges);
+        void CreateOneNumbersArray(SansTopu topu);
+        void UpdateOneNumbersArray(SansTopu topu);
+        void DeleteOneNumbersArray(SansTopu topu);
+    }
+}
---
+++ Repositories/EntityFrameworkCore/SansTopuRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class SansTopuRepository : RepositoryBase<SansTopu>, ISansTopuRepository
+    {
+        public SansTopuRepository(RepositoryContext context) : base(context)
+        {
+
+        }
+    }
+}
