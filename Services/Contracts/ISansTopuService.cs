7e3db66f173bee9aa78c1b90731f5ce01495ef09
Sercan ISLI <sercanislii@gmail.com>
2023-11-23 16:25:50 +0300

    Created ISansTopuService and set method signatures for SansTopu.

---
+++ Services/Contracts/ISansTopuService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+
+namespace Services.Contracts
+{
+    public interface ISansTopuService
+    {
+        IEnumerable<SansTopu> GetAllNumbersArrays(bool trackChanges);
+        SansTopu GetOneNumbersArrayById(int id, bool trackChanges);
+        SansTopu CreateOneNumbersArray(SansTopu sansTopu);
+        void UpdateOneNumbersArray(int id, SansTopu sansTopu);
+        void DeleteOneNumbersArray(int id, bool trackChanges;
+    }
+}
