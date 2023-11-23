df9c2639adf584a71415734c1d78722af2bc604c
Sercan ISLI <sercanislii@gmail.com>
2023-11-23 16:07:32 +0300

    SansTopuRepository was created.

--- Repositories/Contract/ISansTopuRepository.cs
+++ Repositories/Contract/ISansTopuRepository.cs
@@ -6,9 +6,9 @@ namespace Repositories.Contract
     public interface ISansTopuRepository : IRepositoryBase<SansTopu>
     {
         IQueryable<SansTopu> GetAllNumbersArray(bool trackChanges);
-        SansTopu GetOneNumbersArrayById(bool trackChanges);
-        void CreateOneNumbersArray(SansTopu topu);
-        void UpdateOneNumbersArray(SansTopu topu);
-        void DeleteOneNumbersArray(SansTopu topu);
+        SansTopu GetOneNumbersArrayById(int id, bool trackChanges);
+        void CreateOneNumbersArray(SansTopu sansTopu);
+        void UpdateOneNumbersArray(SansTopu sansTopu);
+        void DeleteOneNumbersArray(SansTopu sansTopu);
     }
 }
--- Repositories/EntityFrameworkCore/SansTopuRepository.cs
+++ Repositories/EntityFrameworkCore/SansTopuRepository.cs
@@ -9,5 +9,17 @@ namespace Repositories.EntityFrameworkCore
         {
 
         }
+
+        public void CreateOneNumbersArray(SansTopu sansTopu) => Create(sansTopu);
+
+        public void DeleteOneNumbersArray(SansTopu sansTopu) => Delete(sansTopu);
+
+        public IQueryable<SansTopu> GetAllNumbersArray(bool trackChanges) =>
+            FindAll(trackChanges).OrderBy(st=>st.Date);
+
+        public SansTopu GetOneNumbersArrayById(int id, bool trackChanges) =>
+            FindByCondition(st => st.Id.Equals(id), trackChanges).SingleOrDefault();
+
+        public void UpdateOneNumbersArray(SansTopu sansTopu) => Update(sansTopu);
     }
 }
