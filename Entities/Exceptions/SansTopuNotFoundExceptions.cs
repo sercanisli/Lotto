2e71f639bf9461220ba76a1a5376a0ac2a7a89e6
Sercan ISLI <sercanislii@gmail.com>
2023-11-25 01:34:46 +0300

    SansTopuNotFoundExceptions class was created.

---
+++ Entities/Exceptions/SansTopuNotFoundExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class SansTopuNotFoundExceptions : NotFoundException
+    {
+        public SansTopuNotFoundExceptions(int id) : base($"Sans Topu with id : {id} could not found.")
+        {
+        }
+    }
+}
