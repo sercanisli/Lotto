cf26df2dce087f1ab92764ab3de1e1095dd5de63
Sercan ISLI <sercanislii@gmail.com>
2023-11-27 01:32:54 +0300

    Refactoring in Repositories layer for Pagination

--- Repositories/Contract/ISansTopuRepository.cs
+++ Repositories/Contract/ISansTopuRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Repositories.Cantracts;
 
 namespace Repositories.Contract
 {
     public interface ISansTopuRepository : IRepositoryBase<SansTopu>
     {
-        Task<IEnumerable<SansTopu>> GetAllNumbersArrayAsync(bool trackChanges);
+        Task<IEnumerable<SansTopu>> GetAllNumbersArrayAsync(SansTopuParameters sansTopuParameters ,bool trackChanges);
         Task<SansTopu> GetOneNumbersArrayByIdAsync(int id, bool trackChanges);
         void CreateOneNumbersArray(SansTopu sansTopu);
         void UpdateOneNumbersArray(SansTopu sansTopu);
--- Repositories/EntityFrameworkCore/SansTopuRepository.cs
+++ Repositories/EntityFrameworkCore/SansTopuRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Contract;
 
@@ -15,8 +16,12 @@ namespace Repositories.EntityFrameworkCore
 
         public void DeleteOneNumbersArray(SansTopu sansTopu) => Delete(sansTopu);
 
-        public async Task<IEnumerable<SansTopu>> GetAllNumbersArrayAsync(bool trackChanges) =>
-           await FindAll(trackChanges).OrderBy(st=>st.Date).ToListAsync();
+        public async Task<IEnumerable<SansTopu>> GetAllNumbersArrayAsync(SansTopuParameters sansTopuParameters, bool trackChanges) =>
+           await FindAll(trackChanges)
+            .OrderBy(st=>st.Date)
+            .Skip((sansTopuParameters.PageNumber-1)*sansTopuParameters.PageSize)
+            .Take(sansTopuParameters.PageSize)
+            .ToListAsync();
 
         public async Task<SansTopu> GetOneNumbersArrayByIdAsync(int id, bool trackChanges) =>
             await FindByCondition(st => st.Id == id, trackChanges).SingleOrDefaultAsync();
