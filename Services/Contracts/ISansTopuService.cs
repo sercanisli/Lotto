ad2948cb26a8bc368cee543c64d76830727298b0
Sercan ISLI <sercanislii@gmail.com>
2023-12-01 01:44:52 +0300

    GetOneNumbersArrayByDateAsync signature created for ISansTopuService.

--- Services/Contracts/ISansTopuService.cs
+++ Services/Contracts/ISansTopuService.cs
@@ -9,6 +9,7 @@ namespace Services.Contracts
     {
         Task<(LinkResponse linkResponse, MetaData metaData)> GetAllNumbersArraysAsync(LinkParameters<SansTopuParameters> linkParameters, bool trackChanges);
         Task<SansTopuDto> GetOneNumbersArrayByIdAsync(int id, bool trackChanges);
+        Task<SansTopuDto> GetOneNumbersArrayByDateAsync(DateTime date, bool trackChanges);
         Task<SansTopuDto> CreateOneNumbersArrayAsync(SansTopuDtoForInsertion sansTopuDtoForInsertion);
         Task UpdateOneNumbersArrayAsync(int id, SansTopuDtoForUpdate sansTopuDtoForUpdate, bool trackChanges);
         Task DeleteOneNumbersArrayAsync(int id, bool trackChanges);
