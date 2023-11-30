a22e7dfe83bf4bc78033d1f5a47abe0aa4a618fb
Sercan ISLI <sercanislii@gmail.com>
2023-11-30 17:04:18 +0300

    ISansTopu Links interface was created.

---
+++ Services/Contracts/ISansTopuLinks.cs
@@ -0,0 +1,11 @@
+﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+
+namespace Services.Contracts
+{
+    public interface ISansTopuLinks
+    {
+        LinkResponse TryGenerateLinks(IEnumerable<SansTopuDto> sansTopuDtos, string fields, HttpContext context);
+    }
+}
