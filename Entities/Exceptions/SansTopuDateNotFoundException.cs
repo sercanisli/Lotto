94c878eb4d3a9a0c4d4680ed18719bd9aa219fb2
Sercan ISLI <sercanislii@gmail.com>
2023-12-01 01:54:10 +0300

    SansTopuDateNotFoundException was created.

---
+++ Entities/Exceptions/SansTopuDateNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    internal class SansTopuDateNotFoundException : NotFoundException
+    {
+        public SansTopuDateNotFoundException(DateTime date) : base($"Sans Topu with date : {date} could not found.")
+        {
+        }
+    }
+}
