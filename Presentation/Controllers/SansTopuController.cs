986088f71a2eef48fa836a286cb7aa14e9e3e3bf
Sercan ISLI <sercanislii@gmail.com>
2023-12-01 01:37:58 +0300

    HttpOptions support has been implemented for SansTopu.

--- Presentation/Controllers/SansTopuController.cs
+++ Presentation/Controllers/SansTopuController.cs
@@ -72,5 +72,12 @@ namespace Presentation.Controllers
             await _manager.SansTopuService.DeleteOneNumbersArrayAsync(id, false);
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetSansTopuOptions()
+        {
+            Response.Headers.Add("Allow", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
+            return Ok();
+        }
     }
 }
