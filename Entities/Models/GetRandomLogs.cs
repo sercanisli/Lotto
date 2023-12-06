910f138a74807c57a5287af34c2978ef5fce2a84
Sercan ISLI <sercanislii@gmail.com>
2023-12-07 00:51:28 +0300

    GetRandomLogs was created.

---
+++ Entities/Models/GetRandomLogs.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities.Models
+{
+    public class GetRandomLogs
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public List<int> RandomNumbers { get; set; }
+
+
+        public GetRandomLogs()
+        {
+            RandomNumbers = new List<int>();
+        }
+    }
+}
