d5fb90a26c241cccf90fb5a24588e7fecc385de6
Sercan ISLI <sercanislii@gmail.com>
2023-12-07 18:07:23 +0300

    ISansTopuGetRandomLogsRepository interface and SansTopuGetRandomLogsRepository class was created.

---
+++ Repositories/Contract/ISansTopuGetRandomLogsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Models;
+using Repositories.Cantracts;
+
+namespace Repositories.Contract
+{
+    public interface ISansTopuGetRandomLogsRepository :IRepositoryBase<SansTopuGetRandomLogs>
+    {
+    }
+}
