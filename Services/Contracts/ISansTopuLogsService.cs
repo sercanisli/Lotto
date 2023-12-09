5ac9291ef85b926cbfb647ce22d3fc5db10d64c0
Sercan ISLI <sercanislii@gmail.com>
2023-12-10 01:39:50 +0300

    ISansTopuLogsService was created and signatures are applied.

---
+++ Services/Contracts/ISansTopuLogsService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.DataTransferObjects;
+
+namespace Services.Contracts
+{
+    public interface ISansTopuLogsService
+    {
+        Task<IEnumerable<SansTopuDtoForRandom>> GetAllLogsAsync();
+        Task<SansTopuDtoForRandom> CreateLog(SansTopuDtoForRandom sansTopuDtoForRandom);
+    }
+}
