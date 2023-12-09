b109c2448d3ac9b07df600ca34e48c4d304166d1
Sercan ISLI <sercanislii@gmail.com>
2023-12-10 01:26:21 +0300

    LogsRepositoryBase was created and create and FindAll methods applied.

---
+++ Repositories/EntityFrameworkCore/LogsRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public abstract class LogsRepositoryBase<T> : ILogsRepositoryBase<T> where T : class
+    {
+        private readonly RepositoryContext _context;
+
+        protected LogsRepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity) => _context.Set<T>().Add(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? _context.Set<T>().AsNoTracking() : _context.Set<T>();
+
+    }
+}
