7edb4a029980e3fd400775236a2ff15ce6097932
Sercan ISLI <sercanislii@gmail.com>
2023-12-13 01:52:55 +0300

    Migrations has been applied.

---
+++ WebApi/Migrations/20231212224844_IntialCreateForOnNumaraLogs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialCreateForOnNumaraLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "737efeae-6b9d-414f-9f1b-ff31c80efa97");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "abd5c78f-a795-4d24-ba2a-792e2f522937");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e8f56aa4-d4a5-48d8-832e-fa50c6fb78fe");
+
+            migrationBuilder.CreateTable(
+                name: "OnNumaraLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RandomNumbers = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OnNumaraLogs", x => x.Id);
+                });
+
+           
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2023, 12, 13, 1, 48, 43, 590, DateTimeKind.Local).AddTicks(4602));
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 12, 13, 1, 48, 43, 590, DateTimeKind.Local).AddTicks(4611));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OnNumaraLogs");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3dea0827-2703-4a7f-a26f-7fed66ef33f6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7af0aefd-4476-4ce1-824d-d83245085d3b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a6a36b3a-1f74-492b-a980-7a816be33105");
+
+          
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2023, 12, 10, 3, 18, 40, 245, DateTimeKind.Local).AddTicks(1073));
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 12, 10, 3, 18, 40, 245, DateTimeKind.Local).AddTicks(1084));
+        }
+    }
+}
