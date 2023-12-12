cf134d6efcb807bada76b72223272873edf41f06
Sercan ISLI <sercanislii@gmail.com>
2023-12-13 01:59:17 +0300

    OnNumaraLogsRepository was created.

---
+++ Repositories/EntityFrameworkCore/OnNumaraLogsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.LogModels;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class OnNumaraLogsRepository : LogsRepositoryBase<OnNumaraLogs>, IOnNumaraLogsRepository
+    {
+        public OnNumaraLogsRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateLog(OnNumaraLogs onNumaraLogs) => Create(onNumaraLogs);
+
+        public async Task<IEnumerable<OnNumaraLogs>> GetAllLogsAsync(bool trackChanges) =>
+            await FindAll(trackChanges).ToListAsync();
+    }
+}
