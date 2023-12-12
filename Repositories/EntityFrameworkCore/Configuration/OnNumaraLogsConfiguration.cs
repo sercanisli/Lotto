b0ff38cf2630279a56df800d7f10fdb9edeadea8
Sercan ISLI <sercanislii@gmail.com>
2023-12-13 01:43:30 +0300

    OnNumaraLogsConfiguration was created.

---
+++ Repositories/EntityFrameworkCore/Configuration/OnNumaraLogsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Entities.LogModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.EntityFrameworkCore.Configuration
+{
+    public class OnNumaraLogsConfiguration : IEntityTypeConfiguration<OnNumaraLogs>
+    {
+        public void Configure(EntityTypeBuilder<OnNumaraLogs> builder)
+        {
+            builder.ToTable("OnNumaraLogs").HasKey(s => s.Id);
+            builder.Property(s => s.RandomNumbers)
+                .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
+            )
+            .HasColumnName("RandomNumbers");
+        }
+    }
+}
