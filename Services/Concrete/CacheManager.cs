d7af3fb4b5b7a1c1aaf9cde4eccb434939a6adc2
Sercan ISLI <sercanislii@gmail.com>
2023-12-15 16:55:46 +0300

    GetData has been implemented in CacheManager for Caching.

---
+++ Services/Concrete/CacheManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Services.Concrete
+{
+    public class CacheManager : ICacheService
+    {
+        private IDatabase _cacheDb;
+        IConfiguration configuration;
+
+        public CacheManager()
+        {
+            var redis = ConnectionMultiplexer.Connect(configuration.GetConnectionString("redis"));
+            _cacheDb = redis.GetDatabase();
+        }
+
+        public T GetData<T>(string key)
+        {
+            var value = _cacheDb.StringGet(key);
+            if(!string.IsNullOrEmpty(value))
+            {
+                return JsonSerializer.Deserialize<T>(value);
+            }
+            return default;
+        }
+
+        public object RemoveData(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
