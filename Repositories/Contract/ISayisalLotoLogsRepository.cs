2642eadc461c6af5d3c06c55a94b0abfa70cb613
Sercan ISLI <sercanislii@gmail.com>
2023-12-30 02:22:37 +0300

    ISayisalLotoLogsRepository and methods signatures created.

---
+++ Repositories/Contract/ISayisalLotoLogsRepository.cs
@@ -0,0 +1,10 @@
+﻿using Entities.LogModels;
+
+namespace Repositories.Contract
+{
+    public interface ISayisalLotoLogsRepository
+    {
+        Task<IEnumerable<SayisalLotoLogs>> GetAllLogsAsync(bool trackChanges);
+        void CreateLog(SayisalLotoLogs sayisalLotoLogs);
+    }
+}
