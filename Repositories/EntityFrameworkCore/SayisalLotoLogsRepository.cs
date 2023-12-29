85153cabf3d830ccbb53e16cf94156426ce35a82
Sercan ISLI <sercanislii@gmail.com>
2023-12-30 02:29:38 +0300

    SayisalLotoLogsRepository class created.

---
+++ Repositories/EntityFrameworkCore/SayisalLotoLogsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.LogModels;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class SayisalLotoLogsRepository : RepositoryBase<SayisalLotoLogs>, ISayisalLotoLogsRepository
+    {
+        public SayisalLotoLogsRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateLog(SayisalLotoLogs sayisalLotoLogs) => Create(sayisalLotoLogs);
+
+        public async Task<IEnumerable<SayisalLotoLogs>> GetAllLogsAsync(bool trackChanges) =>
+            await FindAll(trackChanges).ToListAsync();
+    }
+}
