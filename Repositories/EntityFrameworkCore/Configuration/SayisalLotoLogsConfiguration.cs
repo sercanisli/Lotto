8d6fe7a12592e2876fa7fc89e74c705da5cd14df
Sercan ISLI <sercanislii@gmail.com>
2023-12-30 02:15:01 +0300

    SayisalLotoLogsConfiguration created.

---
+++ Repositories/EntityFrameworkCore/Configuration/SayisalLotoLogsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Entities.LogModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.EntityFrameworkCore.Configuration
+{
+    public class SayisalLotoLogsConfiguration : IEntityTypeConfiguration<SayisalLotoLogs>
+    {
+        public void Configure(EntityTypeBuilder<SayisalLotoLogs> builder)
+        {
+            builder.ToTable("SayisalLotoLogs").HasKey(s => s.Id);
+            builder.Property(s => s.RandomNumbers)
+                .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
+            )
+            .HasColumnName("RandomNumbers");
+        }
+    }
+}
