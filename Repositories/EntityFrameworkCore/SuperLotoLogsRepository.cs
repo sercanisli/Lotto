23311ee441ec129d2cd78e49293f6bd47f69c542
Sercan ISLI <sercanislii@gmail.com>
2023-12-31 01:32:34 +0300

    SuperLotoLogsRepository created and implemented from LogRepositoryBase class and ISuperLotoLogsRepository interface.

---
+++ Repositories/EntityFrameworkCore/SuperLotoLogsRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.LogModels;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class SuperLotoLogsRepository : LogsRepositoryBase<SuperLotoLogs>, ISuperLotoLogsRepository
+    {
+        public SuperLotoLogsRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateLog(SuperLotoLogs superLotoLogs) => Create(superLotoLogs);
+
+        public async Task<IEnumerable<SuperLotoLogs>> GetAllLogsAsync(bool trackChanges) =>
+            await FindAll(trackChanges).ToListAsync();
+    }
+}
