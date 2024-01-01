1a9b7895ccbc033bda2b44c125215545cf1df297
Sercan ISLI <sercanislii@gmail.com>
2024-01-02 00:25:39 +0300

    Refactoring in OnNumaraDtoForManipulation.

--- Entities/Validations/OnNumaraDtoForManipulation.cs
+++ Entities/Validations/OnNumaraDtoForManipulation.cs
@@ -2,7 +2,7 @@
 {
     public abstract record OnNumaraDtoForManipulation
     {
-        [ListLength(10, 10)]
+        [ListLength(22, 22)]
         [RangeAttribute(1, 80)]
         public List<int> Numbers { get; init; }
         public DateTime Date { get; init; }
