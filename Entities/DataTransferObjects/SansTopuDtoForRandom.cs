6a0282cd9a8d19303ce5631cf2637c64ffeeae8b
Sercan ISLI <sercanislii@gmail.com>
2024-01-06 15:26:30 +0300

    Refactoring in SansTopuDtoForRandom for Date.

--- Entities/DataTransferObjects/SansTopuDtoForRandom.cs
+++ Entities/DataTransferObjects/SansTopuDtoForRandom.cs
@@ -3,6 +3,7 @@
     public record SansTopuDtoForRandom : LotoDtoForRandom
     {
         public string? MatchRate { get; set; }
+        public string? Date { get; set; }
         public int PlusNumber { get; init; }
     }
 }
