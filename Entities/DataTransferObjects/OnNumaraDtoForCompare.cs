3597eca8e14550e3acd3ceecc6132f83db36a80c
Sercan ISLI <sercanislii@gmail.com>
2024-01-13 01:08:34 +0300

    OnNumaraDtoForCompare created for compare process.

---
+++ Entities/DataTransferObjects/OnNumaraDtoForCompare.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Validations;
+
+namespace Entities.DataTransferObjects
+{
+    public record OnNumaraDtoForCompare
+    {
+        [ListLength(22, 22)]
+        [RangeAttribute(1, 80)]
+        public List<int> Numbers { get; init; }
+    }
+}
