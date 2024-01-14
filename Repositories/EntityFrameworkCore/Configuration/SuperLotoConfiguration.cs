b63c8c9aa639c6701e3f1a50c941387086bd035c
Sercan ISLI <sercanislii@gmail.com>
2024-01-14 17:22:42 +0300

    Refactoring in SuperLotoConfiguration.

--- Repositories/EntityFrameworkCore/Configuration/SuperLotoConfiguration.cs
+++ Repositories/EntityFrameworkCore/Configuration/SuperLotoConfiguration.cs
@@ -16,26 +16,6 @@ namespace Repositories.EntityFrameworkCore.Configuration
                 v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()
             )
             .HasColumnName("Numbers");
-            builder.HasData(
-                new SuperLoto
-                {
-                    Id = 1,
-                    Numbers = new List<int> { 45, 26, 17, 6, 27, 60 },
-                    Date = new DateTime(2023, 10, 27)
-                },
-                new SuperLoto
-                {
-                    Id = 2,
-                    Numbers = new List<int> { 25, 7, 9, 17, 27, 42 },
-                    Date = new DateTime(2023, 9, 6)
-                },
-                new SuperLoto
-                {
-                    Id = 3,
-                    Numbers = new List<int> { 12, 4, 17, 6, 27, 60 },
-                    Date = new DateTime(2023, 2, 27)
-                }
-                );
         }
     }
 }
---
+++ WebApi/Migrations/20240114140403_InitialCreateForSuperLoto.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateForSuperLoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "62ff7d8c-a7a9-4474-85fb-45cea82d903c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "92c651c2-5cc4-4783-834d-4235f74d6448");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bbc4b342-0f3f-4d9c-ae7c-cf8c170d089c");
+
+            migrationBuilder.DeleteData(
+                table: "SuperLotos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "SuperLotos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "SuperLotos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 1, 14, 17, 4, 2, 733, DateTimeKind.Local).AddTicks(7189));
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 1, 14, 17, 4, 2, 733, DateTimeKind.Local).AddTicks(7195));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "146a0e7f-0917-4dba-8b1b-108d93a128c3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c57899f3-bb53-464a-b248-df6fcceac275");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d8fd0ca5-165e-4d31-9f52-f9536da09696");
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 1, 13, 16, 8, 16, 488, DateTimeKind.Local).AddTicks(6246));
+
+            migrationBuilder.UpdateData(
+                table: "SansTopuLogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 1, 13, 16, 8, 16, 488, DateTimeKind.Local).AddTicks(6253));
+
+        }
+    }
+}
