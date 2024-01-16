d80b5bdfc10f12d5e5d1bcce1290bb5e1f52dddb
Sercan ISLI <sercanislii@gmail.com>
2024-01-17 01:36:37 +0300

    OnNumaraDtoForCompareWithLogs created.

---
+++ Entities/DataTransferObjects/OnNumaraDtoForCompareWithLogs.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Validations;
+
+namespace Entities.DataTransferObjects
+{
+    public record OnNumaraDtoForCompareWithLogs
+    {
+        [ListLength(22, 22)]
+        [Range(1, 80)]
+        public List<int> Numbers { get; init; }
+    }
+}
