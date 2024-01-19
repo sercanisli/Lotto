15469c62633f0531569b716d619472e862d9539c
Sercan ISLI <sercanislii@gmail.com>
2024-01-20 01:14:37 +0300

    SansTopuDtoForCompareWithLogs created for compare.

---
+++ Entities/DataTransferObjects/SansTopuDtoForCompareWithLogs.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Validations;
+
+namespace Entities.DataTransferObjects
+{
+    public record SansTopuDtoForCompareWithLogs
+    {
+        [ListLength(5, 5)]
+        [RangeAttribute(1, 34)]
+        public List<int> Numbers { get; init; }
+        [RangeAttribute(1, 14)]
+        public int PlusNumber { get; set; }
+    }
+}
