5f5ad5a05aa9df91ff415fdd2b49c230d9d7f1be
Sercan ISLI <sercanislii@gmail.com>
2024-01-22 01:16:46 +0300

    SayisalLotoDto return test was performed for the GetOneNumbersArrayByIdForSayisalLotoAsync method in SayisalLotoController.

---
+++ Lotto.Api.Tests.Unit/ControllerTests/SayisalLotoControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Entities.DataTransferObjects;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Presentation.Controllers;
+using Services.Contracts;
+
+namespace Lotto.Api.Tests.Unit.ControllerTests
+{
+    public class SayisalLotoControllerTest
+    {
+        private readonly SayisalLotoController _sut;
+        private readonly IServiceManager _serviceManager = Substitute.For<IServiceManager>();
+
+        public SayisalLotoControllerTest()
+        {
+            _sut = new(_serviceManager);
+        }
+
+        [Fact]
+        public async Task GetOneNumbersArrayByIdForSayisalLotoAsync_ShouldReturnSayisalLotoDto()
+        {
+            //Arrange
+            var sayisalLotoDto = new SayisalLotoDto()
+            {
+                Id = 9999,
+                Numbers = { 5, 10, 15, 20, 25, 30 },
+                Date = Convert.ToDateTime("27.02.1998 00:00:00")
+            };
+            _serviceManager.SayisalLotoService.GetOneNumbersArrayByIdAsync(9999,false).Returns(sayisalLotoDto);
+
+            //Act
+            var result = (OkObjectResult)await _sut.GetOneNumbersArrayByIdForSayisalLotoAsync(9999);
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
