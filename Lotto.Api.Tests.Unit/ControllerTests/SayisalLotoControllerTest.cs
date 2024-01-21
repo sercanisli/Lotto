a6771e4bdd00ea5ceb953b0d1bcb44343701bc32
Sercan ISLI <sercanislii@gmail.com>
2024-01-22 01:57:58 +0300

    Status Code Ok return test was performed for GetOneNumbersArrayByDateForSayisalLotoAsync method in SayisalLotoController.

--- Lotto.Api.Tests.Unit/ControllerTests/SayisalLotoControllerTest.cs
+++ Lotto.Api.Tests.Unit/ControllerTests/SayisalLotoControllerTest.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using Presentation.Controllers;
 using Services.Contracts;
 
@@ -18,7 +19,7 @@ namespace Lotto.Api.Tests.Unit.ControllerTests
         }
 
         [Fact]
-        public async Task GetOneNumbersArrayByIdForSayisalLotoAsync_ShouldReturnSayisalLotoDto()
+        public async Task GetOneNumbersArrayByIdForSayisalLotoAsync_ShouldReturnOk()
         {
             //Arrange
             var sayisalLotoDto = new SayisalLotoDto()
@@ -31,6 +32,26 @@ namespace Lotto.Api.Tests.Unit.ControllerTests
 
             //Act
             var result = (OkObjectResult)await _sut.GetOneNumbersArrayByIdForSayisalLotoAsync(9999);
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task GetOneNumbersArrayByDateForSayisalLotoAsync_ShouldReturnOk()
+        {
+            //Arrange
+            DateTime date = Convert.ToDateTime("27.02.1998 00:00:00");
+            var sayisalLotoDto = new SayisalLotoDto()
+            {
+                Id = 9999,
+                Numbers = { 5, 10, 15, 20, 25, 30 },
+                Date = date
+            };
+            _serviceManager.SayisalLotoService.GetOneNumbersArrayByDateAsync(date, false).Returns(sayisalLotoDto);
+
+            //Act
+            var result = (OkObjectResult)await _sut.GetOneNumbersArrayByDateForSayisalLotoAsync(date);
 
             //Assert
             result.StatusCode.Should().Be(200);
