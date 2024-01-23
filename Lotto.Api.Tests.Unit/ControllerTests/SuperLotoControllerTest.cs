2e5fd9ea9fad731fca17d4e237f22b648c8db4ec
Sercan ISLI <sercanislii@gmail.com>
2024-01-24 01:13:25 +0300

    Status Code Created return tested for CreateOneNumbersArrayForSuperLotoAsync method.

--- Lotto.Api.Tests.Unit/ControllerTests/SuperLotoControllerTest.cs
+++ Lotto.Api.Tests.Unit/ControllerTests/SuperLotoControllerTest.cs
@@ -57,5 +57,33 @@ namespace Lotto.Api.Tests.Unit.ControllerTests
             //Assert
             result.StatusCode.Should().Be(200);
         }
+
+        [Fact]
+        public async Task CreateOneNumbersArrayForSuperLotoAsync_ShouldReturnOk()
+        {
+            //Arrange
+            var numbers = new List<int> { 5, 10, 15, 20, 25, 30 };
+
+            var superLotoDtoForInsertion = new SuperLotoDtoForInsertion()
+            {
+                Numbers = numbers,
+                Date = "27.02.1998 00:00:00"
+            };
+
+            var superLotoDto = new SuperLotoDto()
+            {
+                Id = 9999,
+                Numbers = numbers,
+                Date = Convert.ToDateTime("27.02.1998 00:00:00")
+            };
+
+            _serviceManager.SuperLotoService.CreateOneNumbersArrayAsync(superLotoDtoForInsertion).Returns(superLotoDto);
+
+            //Act
+            var result = (ObjectResult)await _sut.CreateOneNumbersArrayForSuperLotoAsync(superLotoDtoForInsertion);
+
+            //Assert
+            result.StatusCode.Should().Be(201);
+        }
     }
 }
