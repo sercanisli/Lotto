d8eac59c77ed65fdaa36d8936b629689e6d0d89e
Sercan ISLI <sercanislii@gmail.com>
2024-01-25 01:19:25 +0300

    Status code created return tested for the CreateOneNumbersArrayForSansTopuAsync method.

--- Lotto.Api.Tests.Unit/ControllerTests/SansTopuControllerTest.cs
+++ Lotto.Api.Tests.Unit/ControllerTests/SansTopuControllerTest.cs
@@ -61,6 +61,36 @@ namespace Lotto.Api.Tests.Unit.ControllerTests
             result.StatusCode.Should().Be(200);
         }
 
+        [Fact]
+        public async Task CreateOneNumbersArrayForSansTopuAsync_ShouldReturnCreated()
+        {
+            //Arrange
+            var numbers = new List<int> { 5, 10, 15, 20, 25, };
+            int plusNumber = 1;
+
+            var sansTopuDtoForInsertion = new SansTopuDtoForInsertion()
+            {
+                Numbers = numbers,
+                PlusNumber = plusNumber,
+                Date = "27.02.1998 00:00:00"
+            };
+
+            var sansTopuDto = new SansTopuDto()
+            {
+                Id = 9999,
+                Numbers = numbers,
+                PlusNumber = plusNumber,
+                Date = Convert.ToDateTime("27.02.1998 00:00:00")
+            };
+            _serviceManager.SansTopuService.CreateOneNumbersArrayAsync(sansTopuDtoForInsertion).Returns(sansTopuDto);
+
+            //Act
+            var result = (ObjectResult)await _sut.CreateOneNumbersArrayForSansTopuAsync(sansTopuDtoForInsertion);
+
+            //Assert
+            result.StatusCode.Should().Be(201);
+        }
+
         
     }
 }
