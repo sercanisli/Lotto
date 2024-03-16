66cddbb93b9ccb9e0727e6deaac920d14f9da496
Sercan ISLI <sercanislii@gmail.com>
2024-03-16 15:27:07 +0300

    WinningNumbers entity created.

---
+++ Entities/Models/WinningNumbers.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Models
+{
+    public class WinningNumbers
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
