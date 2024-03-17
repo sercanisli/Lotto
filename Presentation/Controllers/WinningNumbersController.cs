1fe98e1bb0ebf378e52196074bff6b0e460d71ea
Sercan ISLI <sercanislii@gmail.com>
2024-03-17 18:43:29 +0300

    CreateOneWinningNumbersAsync method created in WinningNumbersController.

---
+++ Presentation/Controllers/WinningNumbersController.cs
@@ -0,0 +1,35 @@
+﻿using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Services.Contracts;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/winningnumbers")]
+    [ServiceFilter(typeof(ValidateMediaTypeAttribute))]
+    public class WinningNumbersController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public WinningNumbersController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneWinningNumbersAsync([FromRoute(Name = "id")]int id)
+        {
+            var entity = await _manager.WinningNumbersService.GetOneWinningNumbersAsync(id, false);
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOneWinningNumbersAsync([FromBody] WinnigNumbersDto winnigNumbersDto)
+        {
+            var entity = await _manager.WinningNumbersService.CreateOneWinningNumbersAsync(winnigNumbersDto);
+            return StatusCode(201, entity);
+        }
+
+    }
+}
