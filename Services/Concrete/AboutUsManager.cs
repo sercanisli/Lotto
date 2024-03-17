1a3d0dc2e73b9c1ac6cd97f4e545ab129674c5a8
Sercan ISLI <sercanislii@gmail.com>
2024-03-18 01:46:06 +0300

    Service created for About Us

---
+++ Services/Concrete/AboutUsManager.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Cantracts;
+using Services.Contracts;
+
+namespace Services.Concrete
+{
+    public class AboutUsManager : IAboutUsService
+    {
+        private readonly IRepositoryManager _manager;
+        private readonly IMapper _mapper;
+        private readonly ILoggerService _logger;
+
+        public AboutUsManager(IRepositoryManager manager, IMapper mapper, ILoggerService logger)
+        {
+            _manager = manager;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<AboutUsDto> CreateOneAboutUsAsync(AboutUsDto aboutUsDto)
+        {
+            var entity = _mapper.Map<AboutUs>(aboutUsDto);
+            _manager.AboutUs.Create(entity);
+            await _manager.SaveAsync();
+            _logger.LogInfo($"{entity.Id} - About Us Description added to DB");
+            return _mapper.Map<AboutUsDto>(entity);
+        }
+
+        public async Task DeleteAboutUsAsync(int id, bool trackChanges)
+        {
+            var entity = await GetOneAboutUsAsync(id, trackChanges);
+            if (entity == null)
+            {
+                throw new Exception($"About Us with id {id} could not found.");
+            }
+            var mappedEntity = _mapper.Map<AboutUs>(entity);
+            _manager.AboutUs.Delete(mappedEntity);
+            _logger.LogInfo($"About Us with id : {id} deleted");
+            await _manager.SaveAsync();
+        }
+
+        public async Task<AboutUsDto> GetOneAboutUsAsync(int id, bool trackchanges)
+        {
+            var entity = await _manager.AboutUs.FindByCondition(au => au.Id == id, trackchanges).SingleOrDefaultAsync();
+            if(entity == null)
+            {
+                throw new Exception($"About Us with id {id} could not found.");
+            }
+            _logger.LogInfo($"Get request made to About Us with id : {id} ");
+            return _mapper.Map<AboutUsDto>(entity);
+        }
+
+        public async Task UpdateAboutUsAsync(int id, AboutUsDto aboutUsDto, bool trackchanges)
+        {
+            var entity = await GetOneAboutUsAsync(id, trackchanges);
+            if (entity == null)
+            {
+                throw new Exception($"about Us with id : {id} could not found");
+            }
+            var mappedEntity = _mapper.Map<AboutUs>(entity);
+            _manager.AboutUs.Update(mappedEntity);
+            _logger.LogInfo($"About Us with id : {id} has been updated");
+            await _manager.SaveAsync();
+        }
+    }
+}
---
+++ Services/Contracts/IAboutUsService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.DataTransferObjects;
+
+namespace Services.Contracts
+{
+    public interface IAboutUsService 
+    {
+        Task<AboutUsDto> GetOneAboutUsAsync(int id, bool trackchanges);
+        Task<AboutUsDto> CreateOneAboutUsAsync(AboutUsDto aboutUsDto);
+        Task UpdateAboutUsAsync(int id, AboutUsDto aboutUsDto, bool trackchanges);
+        Task DeleteAboutUsAsync(int id, bool trackChanges);
+    }
+}
