0a41bf411ffb8c45fe322629c9b5514af2402e0c
Sercan ISLI <sercanislii@gmail.com>
2024-03-18 01:14:41 +0300

    Repository created for AboutUs

---
+++ Repositories/EntityFrameworkCore/AboutUsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Models;
+using Repositories.Contract;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class AboutUsRepository : RepositoryBase<AboutUs>, IAboutUsRepository
+    {
+        public AboutUsRepository(RepositoryContext context) : base(context)
+        {
+        }
+    }
+}
