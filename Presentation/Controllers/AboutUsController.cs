f980ee466486e35a38ba49565d5941752a3de518
Sercan ISLI <sercanislii@gmail.com>
2024-03-19 01:46:15 +0300

    GetOneAboutUs method created in AboutUsController.

---
+++ Presentation/Controllers/AboutUsController.cs
@@ -0,0 +1,24 @@
+﻿using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/aboutus")]
+    public class AboutUsController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public AboutUsController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOneAboutUs([FromRoute(Name = "id")] int id)
+        {
+            var entity = await _manager.AboutUsService.GetOneAboutUsAsync(id, false);
+            return Ok(entity);
+        }    }
+}
